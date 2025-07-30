abd9fe7b1fd28787efb1a5bd92609f0a27ec3a4d
Wim Deblauwe <wim.deblauwe@gmail.com>
2025-07-30 08:35:30 +0200

    test: Avoid Mockito for VisitControllerTest

--- src/test/java/com/wimdeblauwe/petclinic/visit/web/VisitControllerTest.java
+++ src/test/java/com/wimdeblauwe/petclinic/visit/web/VisitControllerTest.java
@@ -1,24 +1,27 @@
 package com.wimdeblauwe.petclinic.visit.web;
 
-import com.wimdeblauwe.petclinic.owner.OwnerId;
-import com.wimdeblauwe.petclinic.owner.PetId;
-import com.wimdeblauwe.petclinic.veterinarian.VeterinarianId;
-import com.wimdeblauwe.petclinic.visit.Visit;
-import com.wimdeblauwe.petclinic.visit.VisitId;
+import com.wimdeblauwe.petclinic.owner.Owner;
+import com.wimdeblauwe.petclinic.owner.OwnerMother;
+import com.wimdeblauwe.petclinic.owner.Pet;
+import com.wimdeblauwe.petclinic.owner.PetMother;
+import com.wimdeblauwe.petclinic.owner.repository.InMemoryOwnerRepository;
+import com.wimdeblauwe.petclinic.owner.repository.OwnerRepository;
+import com.wimdeblauwe.petclinic.veterinarian.Veterinarian;
+import com.wimdeblauwe.petclinic.veterinarian.VeterinarianMother;
+import com.wimdeblauwe.petclinic.veterinarian.repository.InMemoryVeterinarianRepository;
+import com.wimdeblauwe.petclinic.veterinarian.repository.VeterinarianRepository;
+import com.wimdeblauwe.petclinic.visit.repository.InMemoryVisitRepository;
+import com.wimdeblauwe.petclinic.visit.repository.VisitRepository;
 import com.wimdeblauwe.petclinic.visit.usecase.PlanVisit;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.Instant;
-import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,112 +29,113 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(VisitController.class)
 class VisitControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @MockitoBean
-    private PlanVisit planVisit;
+  @Autowired
+  private OwnerRepository ownerRepository;
 
-    @Test
-    void testPlanVisit_emptyRequest() throws Exception {
-        UUID visitId = UUID.randomUUID();
-        UUID veterinarianId = UUID.randomUUID();
-        UUID ownerId = UUID.randomUUID();
-        UUID petId = UUID.randomUUID();
-        Instant appointmentTime = Instant.parse("2023-01-15T10:00:00Z");
+  @Autowired
+  private VeterinarianRepository veterinarianRepository;
 
-        Visit visit = new Visit(
-                new VisitId(visitId),
-                new VeterinarianId(veterinarianId),
-                new OwnerId(ownerId),
-                new PetId(petId),
-                appointmentTime
-        );
+  @Test
+  void testPlanVisit_emptyRequest() throws Exception {
+    Pet pet = PetMother.pet().build();
+    Owner owner = OwnerMother.owner()
+        .withPet(pet)
+        .build();
+    Veterinarian veterinarian = VeterinarianMother.veterinarian().build();
+    ownerRepository.save(owner);
+    veterinarianRepository.save(veterinarian);
 
-        when(planVisit.execute(any()))
-                .thenReturn(visit);
-
-        mockMvc.perform(post("/api/visits")
+    mockMvc.perform(post("/api/visits")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
                                      {
-
+                                     
                                      }
                                      """))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("code").value("VALIDATION_FAILED"))
-                .andExpect(jsonPath("fieldErrors[*].code", Matchers.hasItems("REQUIRED_NOT_NULL", "REQUIRED_NOT_NULL", "REQUIRED_NOT_NULL", "REQUIRED_NOT_NULL")))
-                .andExpect(jsonPath("fieldErrors[*].property", Matchers.hasItems("veterinarianId", "ownerId", "petId", "appointmentTime")));
-    }
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("code").value("VALIDATION_FAILED"))
+        .andExpect(jsonPath("fieldErrors[*].code", Matchers.hasItems("REQUIRED_NOT_NULL", "REQUIRED_NOT_NULL", "REQUIRED_NOT_NULL", "REQUIRED_NOT_NULL")))
+        .andExpect(jsonPath("fieldErrors[*].property", Matchers.hasItems("veterinarianId", "ownerId", "petId", "appointmentTime")));
+  }
 
-    @Test
-    void testPlanVisit_missingAppointmentTime() throws Exception {
-        UUID visitId = UUID.randomUUID();
-        UUID veterinarianId = UUID.randomUUID();
-        UUID ownerId = UUID.randomUUID();
-        UUID petId = UUID.randomUUID();
-        Instant appointmentTime = Instant.parse("2023-01-15T10:00:00Z");
+  @Test
+  void testPlanVisit_missingAppointmentTime() throws Exception {
+    Pet pet = PetMother.pet().build();
+    Owner owner = OwnerMother.owner()
+        .withPet(pet)
+        .build();
+    Veterinarian veterinarian = VeterinarianMother.veterinarian().build();
+    ownerRepository.save(owner);
+    veterinarianRepository.save(veterinarian);
 
-        Visit visit = new Visit(
-                new VisitId(visitId),
-                new VeterinarianId(veterinarianId),
-                new OwnerId(ownerId),
-                new PetId(petId),
-                appointmentTime
-        );
-
-        when(planVisit.execute(any()))
-                .thenReturn(visit);
-
-        mockMvc.perform(post("/api/visits")
+    mockMvc.perform(post("/api/visits")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(String.format("""
-                                     {
-                                        "veterinarianId": "%s",
-                                        "ownerId": "%s",
-                                        "petId": "%s"
-                                     }
-                                     """, veterinarianId, ownerId, petId)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("code").value("VALIDATION_FAILED"))
-                .andExpect(jsonPath("fieldErrors[*].code", Matchers.hasItems("REQUIRED_NOT_NULL")))
-                .andExpect(jsonPath("fieldErrors[*].property", Matchers.hasItems("appointmentTime")));
-    }
+                                                   {
+                                                      "veterinarianId": "%s",
+                                                      "ownerId": "%s",
+                                                      "petId": "%s"
+                                                   }
+                                                   """, veterinarian.getId().getId(), owner.getId().getId(), pet.getId().getId())))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("code").value("VALIDATION_FAILED"))
+        .andExpect(jsonPath("fieldErrors[*].code", Matchers.hasItems("REQUIRED_NOT_NULL")))
+        .andExpect(jsonPath("fieldErrors[*].property", Matchers.hasItems("appointmentTime")));
+  }
 
-    @Test
-    void testPlanVisit() throws Exception {
-        UUID visitId = UUID.randomUUID();
-        UUID veterinarianId = UUID.randomUUID();
-        UUID ownerId = UUID.randomUUID();
-        UUID petId = UUID.randomUUID();
-        Instant appointmentTime = Instant.parse("2023-01-15T10:00:00Z");
+  @Test
+  void testPlanVisit() throws Exception {
+    Pet pet = PetMother.pet().build();
+    Owner owner = OwnerMother.owner()
+        .withPet(pet)
+        .build();
+    Veterinarian veterinarian = VeterinarianMother.veterinarian().build();
+    ownerRepository.save(owner);
+    veterinarianRepository.save(veterinarian);
 
-        Visit visit = new Visit(
-                new VisitId(visitId),
-                new VeterinarianId(veterinarianId),
-                new OwnerId(ownerId),
-                new PetId(petId),
-                appointmentTime
-        );
-
-        when(planVisit.execute(any()))
-                .thenReturn(visit);
-
-        mockMvc.perform(post("/api/visits")
+    mockMvc.perform(post("/api/visits")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(String.format("""
-                                     {
-                                        "veterinarianId": "%s",
-                                        "ownerId": "%s",
-                                        "petId": "%s",
-                                        "appointmentTime": "2023-01-15T10:00:00Z"
-                                     }
-                                     """, veterinarianId, ownerId, petId)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").value(visitId.toString()))
-                .andExpect(jsonPath("veterinarianId").value(veterinarianId.toString()))
-                .andExpect(jsonPath("ownerId").value(ownerId.toString()))
-                .andExpect(jsonPath("petId").value(petId.toString()))
-                .andExpect(jsonPath("appointmentTime").value("2023-01-15T10:00:00Z"));
+                                                   {
+                                                      "veterinarianId": "%s",
+                                                      "ownerId": "%s",
+                                                      "petId": "%s",
+                                                      "appointmentTime": "2023-01-15T10:00:00Z"
+                                                   }
+                                                   """, veterinarian.getId().getId(), owner.getId().getId(), pet.getId().getId())))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("id").exists())
+        .andExpect(jsonPath("veterinarianId").value(veterinarian.getId().asString()))
+        .andExpect(jsonPath("ownerId").value(owner.getId().asString()))
+        .andExpect(jsonPath("petId").value(pet.getId().asString()))
+        .andExpect(jsonPath("appointmentTime").value("2023-01-15T10:00:00Z"));
+  }
+
+  @TestConfiguration
+  static class TestConfig {
+    @Bean
+    public PlanVisit planVisit(VisitRepository visitRepository,
+                               VeterinarianRepository veterinarianRepository,
+                               OwnerRepository ownerRepository) {
+      return new PlanVisit(visitRepository, veterinarianRepository, ownerRepository);
     }
+
+    @Bean
+    public VisitRepository visitRepository() {
+      return new InMemoryVisitRepository();
+    }
+
+    @Bean
+    public VeterinarianRepository veterinarianRepository() {
+      return new InMemoryVeterinarianRepository();
+    }
+
+    @Bean
+    public OwnerRepository ownerRepository() {
+      return new InMemoryOwnerRepository();
+    }
+  }
 }
